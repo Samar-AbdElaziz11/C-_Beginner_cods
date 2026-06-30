832933d1682c9eb7f4bcd2bd08608760f76c0e49
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-07-01 01:54:04 +0300

    07-vertical-sum.cpp

---
+++ 07-vertical-sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	
+	int x = 20 , y= 30 , z= 10;
+	
+	cout << x << " + " << endl;
+	cout << y << " + " << endl;
+	cout << z << endl;
+	cout << "_______"<< endl;
+	cout << x + y + z << endl;
+	
+	return 0;
