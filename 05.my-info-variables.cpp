1a1cd6ef8714aad1729e072269d9aaee69687564
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-07-01 01:44:53 +0300

    05.my-info-variables.cpp

---
+++ 05.my-info-variables.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+	
+	string myname = "Samar Abdelaziz";
+	int myAge = 19;
+	string mycity = "ismailia";
+	string mycountry = "egypt";
+	char myGender = 'f';
+	bool mymarried = 0;
+	
+	
+	cout << myname << endl;
+	cout << myAge << endl;
+	cout << mycity << endl;
+	cout << myGender << endl;
+	cout << mymarried << endl;
+	
+	return 0;
+	
+}
