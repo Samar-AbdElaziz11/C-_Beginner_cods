9bf7735bba9fa0ac5718376c746ac49b9556e625
Samar-AbdElaziz11 <rase7363@gmail.com>
2026-07-01 01:31:11 +0300

    03-print.Dear-sir.cpp

---
+++ 03-Print-Dear-sir.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Dear sir\\madam\n";
+    cout << "How are you\n";
+    cout << "my name is \\*Samora\\*. nice to meet you";
+    
+    return 0;
+}
